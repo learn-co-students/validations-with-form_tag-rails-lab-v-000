048dcd1c2dd2279dd0b9838c75483441ebb3801b
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-17 20:14:58 +0000

    completes author form features

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,7 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update]
+  
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
@@ -8,13 +9,33 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+  
+  def edit 
+    
+  end
+  
+  def update
+    if Post.new(post_params).valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
-
+  
+  def set_author
+    @author = Author.find(params[:id])
+  end
   def author_params
     params.permit(:name, :email, :phone_number)
   end
