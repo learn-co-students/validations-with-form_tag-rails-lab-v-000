518e668aa938c9e896704fb17848b847189ddcd4
Drew Pham <drewvpham@gmail.com>
2016-04-27 13:42:05 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,10 +7,17 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
+def edit
+  @author = Author.find(params[:id])
+end
+def create
+  @author = Author.new(author_params)
 
-    redirect_to author_path(@author)
+  if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
