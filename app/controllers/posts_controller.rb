da9ec67a0cb398e7b72e1ad1372c93bb16a69b1b
Nathaniel Miller <dukeoflaser@gmail.com>
2016-05-11 23:16:37 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
@@ -9,10 +24,17 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @post.title = params[:title]
+    @post.category = params[:category]
+    @post.content = params[:content]
 
-    @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
   end
 
   private
