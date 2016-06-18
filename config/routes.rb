1d8d25f0568b73f1afc8451ea20fe47802261bef
m-walker <michaelwalker610@gmail.com>
2016-06-17 21:51:28 -0400

    lab completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get '/authors/new', to: "authors#new", as: "new_author"
+  get '/authors/:id', to: "authors#show", as: "author"
+  post '/authors', to: "authors#create"
+  get '/authors/:id/edit', to: "authors#edit"
+  patch '/authors/:id', to: "authors#update"
+
+  get '/posts/new', to: "posts#new", as: "new_post"
+  get '/posts/:id', to: "posts#show", as: "post"
+  get '/posts/:id/edit', to: "posts#edit", as: "edit_post"
+  patch '/posts/:id', to: "posts#update"
+  post '/posts', to: "posts#create"
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
