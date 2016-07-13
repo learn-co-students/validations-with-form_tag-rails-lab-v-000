21144db0b55cd9611e66ba1ccfdd5ebd637a5d6e
CodeByLine <yleventhal@ymail.com>
2016-07-13 15:51:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :authors, only: [:name, :email, :phone_number]
+  resources :posts, only: [:title, :category, :content]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
