c2b6d571091b03fffcb46f64daf25d7f82d635b1
Jason <jasonkwong11@gmail.com>
2016-08-01 18:57:28 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
+require 'pry'
+
 class PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+
   def show
     @post = Post.find(params[:id])
   end
@@ -8,16 +22,21 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+  set_post!
+   if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
   end
 end
