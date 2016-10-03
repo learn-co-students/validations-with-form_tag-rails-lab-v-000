cd19e20a1e513e52ed7e95560a47eff7ea3afc85
Kathleen <github email address>
2016-10-03 16:40:29 -0400

    Done.

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -39,7 +39,6 @@ RSpec.describe "posts/edit", type: :feature do
     end
 
     it "has error class on bad fields" do
-      binding.pry
       expect(page).to have_css(".field_with_errors input[name=title]")
       expect(page).to have_css(".field_with_errors input[name=category]")
       expect(page).to have_css(".field_with_errors textarea[name=content]")
