3b06c77092a9ab91b6f02a22150c81b634f8edf2
christinechoi <smi77y@gmail.com>
2017-01-01 14:31:39 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,39 @@
 class AuthorsController < ApplicationController
+
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new(author_params)
+  end
+
+
+  def edit
+
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def update
+
+  end
+
+private
+
+  def author_params
+    params.permit(:name, :phone_number, :email)
+  end
+
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  
+  def edit
+
+  end
+
+  def create
+    @post = Post.new(params)
+
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def update
+
+  end
+
+
 end
