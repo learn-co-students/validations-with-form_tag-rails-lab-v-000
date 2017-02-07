6894580b97e7cf8d8d52baea4f65998da27cf7cd
Jon Fougner <jon.fougner@gmail.com>
2017-02-07 17:19:43 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,10 @@
 class AuthorsController < ApplicationController
+  before_action :set_author!, only: [:show, :edit, :update]
+
   def show
-    @author = Author.find(params[:id])
+  end
+
+  def edit
   end
 
   def new
@@ -8,14 +12,29 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def set_author!
+    @author = Author.find(params[:id])
   end
 end
