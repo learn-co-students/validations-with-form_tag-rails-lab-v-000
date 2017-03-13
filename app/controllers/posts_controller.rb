9dc7ece7fb53ffbb1275597a38cdf36fcaa5fa5f
brownielox <mrsmpython@gmail.com>
2017-03-13 13:42:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    edit
+    if @post.update(post_params)
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
