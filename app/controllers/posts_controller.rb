f854207cffb2ec2b2b5b37ef7488d906fadba5bf
Kelsey Huse <kelseyhuse30@gmail.com>
2017-04-24 21:26:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,33 @@
 class PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
+  
   def show
-    @post = Post.find(params[:id])
   end
 
-  def edit
-    @post = Post.find(params[:id])
+  def edit   
   end
 
   def update
-    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
 
-    @post.update(post_params)
+  def new
+    @post = Post.new
+  end
 
-    redirect_to post_path(@post)
+  def create
+    @post = Post.new(post_params)
+
+    if @post.valid?
+      @post = Post.create(post_params)
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   private
@@ -20,4 +35,9 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:title, :category, :content)
   end
+
+  def set_post!
+    @post = Post.find(params[:id])
+  end
+
 end
