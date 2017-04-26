770d5a8b57bddf61f8412a7a68e2bc2617228901
Shmully Wolfson <shmullywolfson@gmail.com>
2017-04-25 22:27:46 -0400

    Done.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe AuthorsController do
     end
     let(:bro_found) { Author.find_by(name: bro_attributes[:name]) }
 
-    before { post :create, bro_attributes }
+    before { post :create, params: bro_attributes }
 
     it "creates successfully" do
       expect(bro_found).to be_a(Author)
@@ -71,7 +71,7 @@ RSpec.describe AuthorsController do
     end
 
     it "renders the form again" do
-      post :create, bad_attributes
+      post :create, params: bad_attributes
       expect(response).to render_template(:new)
     end
   end
