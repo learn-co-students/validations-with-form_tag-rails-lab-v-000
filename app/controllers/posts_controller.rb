ba6152e7d3bf288e3b25f91df82b23a0c073c3da
Aditi Green <asaki444@gmail.com>
2017-06-22 04:59:04 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,12 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
-
+    valid = @post.update(post_params)
+    if valid
     redirect_to post_path(@post)
+    else
+    render :edit
+    end
   end
 
   private
