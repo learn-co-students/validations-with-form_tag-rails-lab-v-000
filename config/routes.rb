e8b4fab118aa50dc0c8136111fb252fac5fdd85d
Samantha Knight <sammay82@yahoo.com>
2017-06-27 23:37:07 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :authors, only: [:index, :new, :create, :edit, :update, :show]
+  resources :posts, only: [:index, :new, :create, :edit, :update, :show]
 end
