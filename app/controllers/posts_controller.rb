536f568542d8841d0ff4f663a5e055a2577eeb3e
Allison Chesky <afc340@nyu.edu>
2017-07-06 02:05:45 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,21 @@
+
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:format])
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:format] || params[:id])
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
