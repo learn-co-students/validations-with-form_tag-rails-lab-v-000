cc129d2517313fc56bdda8c38269d9da27c3ae85
Matteo Piccini <gnappo.1@live.it>
2017-07-07 10:14:06 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :name, presence: true
+  validates :email, uniqueness: true 
+  validates :phone_number, length: { is: 10 }, presence: { message: "must be 10 digits" }, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+  validates :content, length: { minimum: 100 }, presence: { message: "must be at least 100 characters long" }
 end
