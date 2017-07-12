5bc88b1e8b191a483708a0a2cc4f5ec12b912844
curlywallst <nnoyes@comcast.net>
2017-07-12 16:10:11 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,18 +9,6 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  # def update
-  #   @post = Post.new(post_params)
-  #   if @post.valid?
-  #     @post = Post.find(params[:id])
-  #   # if @post.update(post_params)
-  #     @post.update(post_params)
-  #     redirect_to(@post)
-  #   else
-  #     render :edit
-  #   end
-  # end
-
 
   def update
     @post = Post.find(params[:id])
