fb31b5861eb7de9fd425361c6c4122b675838b94
Niki Lee <ew1415048nk@gmail.com>
2017-07-13 20:10:50 +0900

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, :presence => true
+  validates_length_of :content, :minimum => 100
+  validates_inclusion_of :category, :in => %w(Fiction Non-Fiction)
 end
