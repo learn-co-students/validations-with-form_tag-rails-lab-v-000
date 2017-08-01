c5c20869f8dd21f56ec97a6de9ca1541e04daca6
blinded93 <blinded93@gmail.com>
2017-07-31 19:49:51 -0700

    passed tests

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,12 +8,33 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+    set_author
+  end
+
+  def update
+    set_author
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_author
+    @author = Author.find_by(id: params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
