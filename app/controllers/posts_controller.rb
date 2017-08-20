614038a49dc1196f03c84798c8254c2948c6f1a6
Terry Blue <terryblue@comcast.net>
2017-08-20 21:03:06 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,16 +3,31 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.errors.full_messages.size > 0
+      render :new
+    else
+      redirect_to post_path(@post)
+    end
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.errors.full_messages.size > 0
+      render :edit
+    else
+      redirect_to post_path(@post)
+    end
   end
 
   private
