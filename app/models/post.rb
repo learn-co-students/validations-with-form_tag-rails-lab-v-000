392ee3625adf9b32b0d668cea8efa821b5a320be
Aimee Carney <acarney29@gmail.com>
2017-08-24 00:51:51 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} can either be Fiction or Non-Fiction" }
 end
