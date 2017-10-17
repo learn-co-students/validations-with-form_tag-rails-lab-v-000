905c28f5941eb3ddd7601a9072b41bc7e59345e6
laramontana <larabrightside@gmail.com>
2017-10-17 22:28:02 +0300

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,13 +14,17 @@ class PostsController < ApplicationController
       redirect_to post_path(@post)
     else
       render :edit
-    end 
+    end
   end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
