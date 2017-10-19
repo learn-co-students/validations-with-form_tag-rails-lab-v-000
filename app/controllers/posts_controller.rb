496adf54efa2ab6940761ab89091ae73a0217992
Alexandra Wright <superbiscuit@gmail.com>
2017-10-19 16:07:58 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,8 +21,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    test = Post.new(post_params)
-    if test.valid?
+    @post.attributes = (post_params)
+    if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
     else
