658a4ffa63046a76e3e7ad57380a8a2f6913a8d4
Igor Eskin <github email address>
2017-10-29 19:11:25 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,8 +10,7 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-    if @author.save #@author.valid?
-
+    if @author.save 
       redirect_to author_path(@author)
     else
       render :new
