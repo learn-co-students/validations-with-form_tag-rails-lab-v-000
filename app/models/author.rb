25c0a4adff062e64d857ad3bc3b6ea39a06e61ce
jamesonbass1987 <jamesonbass@gmail.com>
2017-11-01 10:56:48 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :phone_number, length: { is: 10 }, presence: true
 end
