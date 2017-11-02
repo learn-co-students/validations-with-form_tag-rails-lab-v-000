10e97d080ae149b93f41b7660fce187c8f92db0c
lpassamano <leighayna.josele@gmail.com>
2017-11-02 18:41:32 -0400

    added Author validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minumum: 100}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
