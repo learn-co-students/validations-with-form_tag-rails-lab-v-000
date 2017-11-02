c104b5a3b3655670cc4ef4c2d81e0c2c215f34c8
lpassamano <leighayna.josele@gmail.com>
2017-11-02 18:49:28 -0400

    added Author routes and #create controller

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :phone_number, length: {is: 10}
-  validates :name, uniqueness: true
+  validates :email, uniqueness: true
 end
