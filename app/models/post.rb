56222289df1719395ca88857920b4f90362f51d3
Scott Wakeman <github email address>
2017-11-08 21:35:55 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ActiveRecord::Base
+
+
+<% if @post.errors.any? %>
+  <div id="error_explanation">
+    <h2><%= pluralize(@post.errors.count, "error") %> prohibited this article from being saved:</h2>
+
+    <ul>
+    <% @post.errors.full_messages.each do |msg| %>
+      <li><%= msg %></li>
+    <% end %>
+    </ul>
+  </div>
+<% end %>
+
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
