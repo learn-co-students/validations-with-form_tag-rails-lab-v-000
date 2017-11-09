0b7615f4c3defcb033279a27077705895a9d132a
Scott Wakeman <github email address>
2017-11-08 21:39:22 -0800

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,17 +1,4 @@
 class Author < ActiveRecord::Base
-
-<% if @author.errors.any? %>
-  <div id="error_explanation">
-    <h2><%= pluralize(@author.errors.count, "error") %> prohibited this article from being saved:</h2>
-
-    <ul>
-    <% @author.errors.full_messages.each do |msg| %>
-      <li><%= msg %></li>
-    <% end %>
-    </ul>
-  </div>
-<% end %>
-
   validates :name, presence: true
   validates :email, uniqueness: true
   validates :phone_number, length: { is: 10 }
