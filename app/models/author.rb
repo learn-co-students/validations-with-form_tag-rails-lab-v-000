0ba85075763d71fdbc78d07dcee1e9462d0c68cd
Mary Benitez <meebenitez@gmail.com>
2017-11-14 19:36:55 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :phone_number, :with => /\d{3}\d{3}\d{4}/
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  CATEGORY_OPTIONS = %w[Fiction Non-Fiction]
+  validates :category, inclusion: { in: CATEGORY_OPTIONS}
 end
