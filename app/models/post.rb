b99a3e0ddbd19243b6ffafb867c2a6e9ce5215d8
github username <github email address>
2017-11-19 18:08:44 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: {message: "cannot be blank"}
+  validates :content, length: {minimum: 100, message: "must be longer than 100 characters"}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Enter either Fiction or Non-Fiction"}
 end
