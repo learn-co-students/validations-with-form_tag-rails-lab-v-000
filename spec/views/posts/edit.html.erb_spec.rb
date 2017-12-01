18d0ad3000757da4eb71be2c3086782849d249ad
Sam Ojling <s.ojling@gmail.com>
2017-12-01 09:20:30 -0800

    Done.

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "posts/edit", type: :feature do
     end
 
     it "renders an error list" do
-      expect(all("#error_explanation li").size).to eq(3)
+      expect(all("#error_explanation li").size).to eq(2)
     end
 
     it "prefills fields" do
