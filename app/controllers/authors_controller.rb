d854594fe0288086c77f83e4633c5eea996fd203
miriam <miriam5000@gmail.com>
2017-12-31 18:35:29 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,6 +11,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
       if @author.save
         redirect_to author_path(@author)
+    
       else 
         render :new
       end 
