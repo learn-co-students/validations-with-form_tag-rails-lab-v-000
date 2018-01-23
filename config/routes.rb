1d4e581d22845809c4d0b64d403bdaabd16bcd12
Mel Rudick <melissarudick@MacBook-Air.local>
2018-01-22 23:06:30 -0600

    errors messages not listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resource :authors
-  resource :posts
+  resources :authors, :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
