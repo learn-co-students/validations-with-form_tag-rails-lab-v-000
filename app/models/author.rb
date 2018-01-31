2127915369da89d15209f063115d003007f037bf
zszalas <github email address>
2018-01-31 09:11:54 -0500

    pushing final

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+  #must have name
+validates :name, presence: true
+#must have unique email
+validates :email, uniqueness: true
+#must have 10 digits
+validates :phone_number, length: {is: 10}
 end
