70091f1526490d714530d688fa0f0e4779ac5c3c
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-03-02 19:34:34 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+     @post = Post.find(params[:id])
   end
 
   def update
+     @post = Post.find(params[:id])
     @post.update(post_params)
     if @post.valid?
     redirect_to post_path(@post)
     else
-    render :update
+    render :edit
     end
   end
   
