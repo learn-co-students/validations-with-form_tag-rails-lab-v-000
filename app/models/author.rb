7153f10c1c8fc3034b3d304075ca612e9b2d0285
funemployeddad <funemployeddad@gmail.com>
2018-03-10 19:04:24 -0700

    save

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,9 @@
 class Author < ActiveRecord::Base
+
   has_many :posts
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :phone_number, length: {is: 10}
   validates :email, uniqueness: true
-  validates :phone_number, length: {minimum: 10}
+
 end
--- app/views/authors/_author.json.jbuilder
+++ app/views/authors/_author.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! author, :id, :created_at, :updated_at
+json.extract! author, :id, :name, :email, :phone_number, :created_at, :updated_at
 json.url author_url(author, format: :json)
