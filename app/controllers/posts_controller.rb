dbb5bc2d8a03df4e993aa81a7b134b1227286e0d
github username <github email address>
2018-04-02 12:21:46 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -10,9 +29,14 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    @post.assign_attributes(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
