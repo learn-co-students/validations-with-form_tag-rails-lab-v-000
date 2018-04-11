a740bc11cfcfd80a50b2b69f779ea6f83519effd
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-04-11 12:55:59 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: {:case_sensitive => false}
+    validates :phone_number, :phone_number => {:ten_digits => true}
 end
