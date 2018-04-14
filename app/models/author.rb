e4ac8725e75bda281c831bac809dbca87815575b
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-14 18:54:55 +0100

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
+# Author model. Must have a name. The phone number must be 10 digits. The email
+# must be unique.
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone_number, length: { is: 10 }
+  validates :email, uniqueness: true
 end
