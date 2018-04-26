41893e9e2f71fe55f1fd6d4910145ff316f5a920
chaimoosh <chaimoosh@gmail.com>
2018-04-26 10:23:04 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: { message: "Must have title" }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Must be Fiction or Non-Fiction"}
+  validates :content, length: { minimum: 100, message: "Must be at least 100 characters" }
 end
