f0035b2552c3d1e1e4e1f1451d8c821e0face038
github username <github email address>
2018-05-15 16:51:06 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: { message: "must have a name" }
+    validates :phone_number, length: { minimum: 10 }
+    validates :email, uniqueness: true
 end
