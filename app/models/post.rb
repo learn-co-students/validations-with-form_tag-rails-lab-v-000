01f5be9a4cca9fd9eceb6b27cc64dbea9388a566
Aly Alidina <aly_alidina@hotmail.com>
2018-05-17 03:33:05 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :name, presence: true
+  validates :title, presence: true
   validates :content, length: {minimum: 100}
-  validated :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
