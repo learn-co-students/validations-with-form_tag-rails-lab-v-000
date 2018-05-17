64c33f784760b02360792f3f56316060c7a94ddb
Aly Alidina <aly_alidina@hotmail.com>
2018-05-17 03:29:46 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :name, presence: true
+  validates :content, length: {minimum: 100}
+  validated :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
