239d9ed82eb1e0dc2f60347a88acf1ee48dc03bf
github username <github email address>
2018-05-20 00:21:28 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:show, :new, :create, :update]
+  resources :authors, only: [:show, :new, :create, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
