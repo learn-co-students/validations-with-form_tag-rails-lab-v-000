0dbb9e5e0e37ec8cc2444e1f4687cfadd5ec7423
Raiet Intraub <raietsaeed@gmail.com>
2018-05-28 03:06:36 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,5 @@
 class AuthorsController < ApplicationController
-  def show
-    @author = Author.find(params[:id])
-  end
+  before_action :set_author, only: [:show, :edit, :update]
 
   def new
     @author = Author.new
@@ -9,13 +7,35 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
   end
+
+  # Referenced Lab : rails-resource-and-scaffold-generator-v-000
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
 end
+
+# rspec spec/controllers/authors_controller_spec.rb
+# rspec spec/views/authors/new.html.erb_spec.rb
