51972eee0406487c74481b11ec05154f6afede1f
ElvinGarcia <dimix181@gmail.com>
2018-07-03 15:23:04 -0400

    update author model to incl. email validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :phone_number, length: {is: 10}
-  validates :name, :phone_number, uniqueness: true
+  validates :name, :phone_number, :email,  uniqueness: true
 
 end
