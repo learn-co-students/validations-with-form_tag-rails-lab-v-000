c9273e2cb196b70994f66e436af12ac3de07341c
ElvinGarcia <dimix181@gmail.com>
2018-07-03 12:49:06 -0400

    post model passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 10}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%(value) is an invalid option" }
+
 end
