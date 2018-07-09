31fdbe33b29fac62cfb0d1c5d8a423a14cd1d59b
ElvinGarcia <dimix181@gmail.com>
2018-07-09 13:35:50 -0400

    cleanup posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
   resource :author
-#  resource :post, except: [:edit,:update, :create]
-#  get   '/post/',         to: 'posts#create', as: 'post'
-#  get   '/post/:id/edit', to: 'posts#edit',   as: 'edit_post'
-#  patch '/post/:id',      to: 'posts#update'
-
-  get   '/posts',         to: 'posts#index', as: 'posts'
-  get   '/posts/new',     to: 'posts#new',   as: 'new_post'
-  post  '/posts',         to: 'posts#create'
-  get   '/posts/:id',     to: 'posts#show', as: 'post'
-  get   '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
-  patch '/posts/:id',     to: 'posts#update'
+  resources :posts, only:[:show, :edit, :update]
 
 
 
