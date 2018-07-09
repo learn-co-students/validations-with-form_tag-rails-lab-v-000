4143bb8ab83797c2dd757522fb443840871f5de6
github username <github email address>
2018-07-08 22:57:28 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,15 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      # re-render the :new template WITHOUT throwing away the invalid @person
+      render :edit
+    end
   end
 
   private
