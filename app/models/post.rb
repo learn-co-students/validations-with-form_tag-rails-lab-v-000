198f252ceb4ff11dd7d4d858a36a11e21869ac98
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-25 14:30:06 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  
+  validates :title, presence: true 
+  validates :category, inclusion: {in: ["Ficiton", "Non-Fiction"]} 
+  validates :content, length: {minimum: 10}
+  
 end
