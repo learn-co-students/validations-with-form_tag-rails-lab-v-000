c06d2a14505fa6ba3dbf17f19f5bb47a5e852242
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-06 11:03:54 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,45 @@
 class AuthorsController < ApplicationController
-  def show
-    @author = Author.find(params[:id])
-  end
 
   def new
     @author = Author.new
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    
+    if @author.valid?
+      @author.save
+      
+      redirect_to author_path(@author)
+      
+    else
+      #binding.pry
+      render :new
+    end
+    
+  end
+  
+  def edit
+    @author = Author.find_by(id: params[:id])
+  end
+  
+  def update
+    @author = Author.find_by(id: params[:id])
+    @author.name = params[:name]
+    @author.email = params[:email]
+    @author.phone_number = params[:phone_number]
+    
+    if @author.valid?
+      @author.update(author_params)
+      redirect_to author_path(@author)
+      
+    else
+      render :edit
+    end
+  end
+  
+  def show
+    @author = Author.find(params[:id])
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,20 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
+  
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.valid?
+      @post.save
+      redirect_to posts_path
+      
+    else
+      render :new
+    end
   end
 
   def edit
@@ -9,10 +23,21 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post.title = params[:title]
+    @post.content = params[:content]
+    @post.category = params[:category]
+    
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+      
+    else
+      render :edit
+    end
+  end
+  
+  def show
+    @post = Post.find(params[:id])
   end
 
   private
