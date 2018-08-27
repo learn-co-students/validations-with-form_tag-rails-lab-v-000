980352f742eff0cc1f5629639364f5d1347aa726
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-08-27 14:10:53 -0700

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,40 @@
 class AuthorsController < ApplicationController
-  def show
-    @author = Author.find(params[:id])
-  end
-
+  
   def new
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+  def show
+    @author = Author.find(params[:id])
   end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])  
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+
+    if @author.update(post_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
+
 
   private
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
+  
+  def new
+    @post = Post.new
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -9,11 +24,15 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
+
 
   private
 
