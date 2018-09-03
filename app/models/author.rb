44bbf414f1988ce14dac8d87c8140c13697b61f9
rfenix3 <richard.fenix@gmail.com>
2018-09-03 13:53:26 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :name
+  validates :email, uniqueness: true
+  validates :phone_number, numericality: {only_integer: true}
+  validates :phone_number, length: { is: 10}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates :content, length: {minimum: 100}
+  validates_inclusion_of :category, :in => %w(Fiction Non-Fiction)
+
 end
