85cbbc3a590ca56071d738ed6dba11e89eed5c9a
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-18 23:52:37 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true, uniqueness: true, length: { is: 10 }
+  validates :phone_number, length: { is: 10 }
 end
