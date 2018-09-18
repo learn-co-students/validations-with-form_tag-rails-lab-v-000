5d4572ea0549e4ee6f01c1d8666f745bb67c5698
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-18 16:40:20 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,15 @@
 class PostsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
   def show
     @post = Post.find(params[:id])
   end
-
+  
   def edit
     @post = Post.find(params[:id])
   end
