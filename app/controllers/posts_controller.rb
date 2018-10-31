d7f332ea1fdf0fbbf1c225b5f4df2fd15e46df31
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-10-31 03:38:46 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,6 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def new
-    @post = Post.new
-  end
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to post_path(@post)
-    else
-      render :new
-    end
-  end
-
   def edit
     @post = Post.find(params[:id])
   end
