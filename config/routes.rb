c1e823dd7119919805282fe119593038c3db72c5
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-21 15:49:00 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  resource :author, only: [:show, :new, :edit, :create, :update]
-  resource :post, only: [:show, :new, :edit, :create, :update]
+  # resource :author, only: [:show, :new, :edit, :create, :update]
+  # resource :post, only: [:show, :new, :edit, :create, :update]
+
+  get "/authors/new", to: "authors#new", as: "new_author"
+  post "/authors", to: "authors#create"
+  get "/authors/:id", to: "authors#show", as: "author"
+  get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
+  patch "/authors/:id", to: "authors#update"
+
+  get "/posts/new", to: "posts#new", as: "new_post"
+  post "/posts", to: "posts#create"
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "/posts/:id", to: "posts#update"
 end
