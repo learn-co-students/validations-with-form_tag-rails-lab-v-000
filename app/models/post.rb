6cb8d70c8eb30451635f4eff47883a50e410397a
amitnyc83 <amit_nyc@live.com>
2018-11-21 20:56:42 -0500

    passing all test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  category_name = ["Fiction", "Non-Fiction"]
+  validates :title, presence: true
+  validates :content, length: {minimum: 100}
+  validates :category, inclusion: category_name
 end
