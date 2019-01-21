7c2330f8cfaa3c7bc85ed89a81ec35f0d73396a6
harleyharl <harleymteden@gmail.com>
2019-01-20 21:27:10 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    binding.pry
     @post = Post.find(params[:id])
+    @post.update(post_params)
     if @post.valid?
-      @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
