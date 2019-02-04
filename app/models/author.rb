e90fe50d556a45eedd1a1a06fd855f966cf0e426
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-02-04 17:13:53 -0500

    Complete lab

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+	
+	validates :name, presence: true
+	validates_uniqueness_of :email
+	# validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+	validates_length_of :phone_number, minimum: 10, maximum: 10
+
 end
