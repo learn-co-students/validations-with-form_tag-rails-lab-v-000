51b621eb5d5ab7c250e5ae7f4d542101c758db3c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-03-01 04:30:37 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
+
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
@@ -8,9 +8,21 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+     if @author.valid?
+       @author.save
+       redirect_to author_path(@author)
+     else
+       render :new
+     end
+  end
 
-    redirect_to author_path(@author)
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
