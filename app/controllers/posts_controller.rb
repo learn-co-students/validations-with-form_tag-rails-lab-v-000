f23824d9de29c51a69eb7c9971ca4a68c44b0c41
Sushi Chanrai <sushi@pathworks.co>
2019-03-06 13:24:32 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,39 @@
 class PostsController < ApplicationController
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
     @post = Post.find(params[:id])
   end
 
+
   def update
     @post = Post.find(params[:id])
-
+    #binding.pry
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    #binding.pry
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
+
 
   private
 
