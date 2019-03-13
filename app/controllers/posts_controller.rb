bb501172c211835cb46ec777f88b0be8a2dd3c6f
Leslie Stevens <flymaven@protonmail.com>
2019-03-12 19:13:27 -0500

    build routes add validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,30 @@
 class PostsController < ApplicationController
+    before_action :set_post!, only[:show, :edit, :update]
+    
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post_params)
+    else
+      render :new
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
@@ -20,4 +32,7 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:title, :category, :content)
   end
+
+  def set_post!
+    @post = Post.find(params[:id])
 end
