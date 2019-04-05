ea4697438903cfc0e5317a0230fc5c1837496adc
Ashley Wong <ashwong04@gmail.com>
2019-04-05 19:44:29 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,9 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    post = Post.new(post_params)
-    if post.valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
-      @post = Post.new(post_params)
       render :edit
     end
   end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-    # message: "Name cannot be blank"
-
   validates :email, uniqueness: true
-    # message: "Email already used"
-
   validates :phone_number, length: {is: 10}
-    # message: "Phone number must be 10 numbers"
 end
