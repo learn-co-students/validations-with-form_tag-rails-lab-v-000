08f50e1f347912ac78bdf359a651072658d875bc
trtoledo <trtoledo1977@gmail.com>
2019-04-06 23:53:21 -0400

    All done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    set_author
   end
 
   def new
@@ -8,12 +8,36 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+    render :new
+    end
   end
 
+  def edit
+    set_author
+  end
+
+  def update
+    set_author
+    @author.update(author_params)
+    if @author.valid?
+       @author.save
+    redirect_to author_path(@author)
+    else
+    render :edit
+    end
+  end
+
+
   private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,42 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
+    set_post
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+    render :new
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
+    set_post
   end
 
   def update
-    @post = Post.find(params[:id])
-
+    set_post
     @post.update(post_params)
-
+    if @post.valid?
+       @post.save
     redirect_to post_path(@post)
+    else
+    render :edit
+    end
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.permit(:title, :category, :content)
