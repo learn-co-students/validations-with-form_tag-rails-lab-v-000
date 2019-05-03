424380cd7213ce8664e14227d2358ba04821e353
github username <github email address>
2019-05-03 13:26:23 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,8 +7,20 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def create
     @author = Author.create(author_params)
+
+    redirect_to author_path(@author)
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    @author.update(author_params)
 
     redirect_to author_path(@author)
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    redirect_to post_path(@post)
   end
 
   def update
