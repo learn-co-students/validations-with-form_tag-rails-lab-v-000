685f5d38d5bc47095cda03e7822201b52c7abd94
github username <github email address>
2019-06-27 00:06:53 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,43 @@
 class AuthorsController < ApplicationController
+  # before_action :set_author!, only: [:show, :edit, :update]
   def show
     @author = Author.find(params[:id])
   end
 
   def new
-    @author = Author.new
+    @author = Author.new(author_params)
+  end
+
+  def edit
+
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
 
-    redirect_to author_path(@author)
   end
+
+  # def update
+  #   if @author.update(author_params)
+  #
+  #   end
+  #
+  # end
+
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
   end
+
+  # def set_author!
+  #   @author = Post.find(param[:id])
+  # end
 end
