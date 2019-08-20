77bd0a5e781ca5f5f338480cce22ed54cd9bc2a2
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-08-20 08:17:11 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+
   def show
     @author = Author.find(params[:id])
   end
@@ -8,14 +9,22 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
   end
 end
