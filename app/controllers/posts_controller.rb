4d5bb95acf007a053a183048967915981df0602c
GiancarloZ <gzabaleta48@gmail.com>
2019-11-23 18:02:05 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,13 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:show, :edit, :update]
+  resources :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
