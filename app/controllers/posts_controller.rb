88db04a87d2f000d998ca3fa9d5c1f7a433d18ed
Tyler Sell <tylerselldesign@gmail.com>
2020-04-02 21:07:19 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,15 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @post.assign_attributes(post_params)
+    
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render :edit 
+    end
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
   end
 
   private
