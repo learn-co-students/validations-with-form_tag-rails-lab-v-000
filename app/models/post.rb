cd8e3c35515abef8e9da6f00535cb7f495741e31
tscotce <tatyana.celovsky@gmail.com>
2020-05-11 17:56:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid selection" }
+    validates :content, length: { minimum: 100, too_short: "%{count} characters is the minimum allowed" } 
 end
